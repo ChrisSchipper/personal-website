def20f767cdbb4f558638880d6e5d80f90b64441
Chris Schipper <cschipper08@gmail.com>
2020-06-13 11:42:29 -0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,78 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col md="7">
+            <div class="title-line">  </div>
+          </v-col>
+          <v-col align-self="end" md="4">
+              <p class="title-text">Christopher Schipper</p>
+          </v-col>
+        </v-row>
+      </v-container>
+      <Index/>
+    </v-main>
+  </v-app>
 </template>
 
+<script lang="ts">
+import Vue from 'vue';
+import Index from './components/Index.vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+    Index,
+  },
+
+  data: () => ({
+    //
+  }),
+});
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import 'src/sass/variables.scss';
+
+.title-text {
+  display: flex;
+  width: 100%;
+  opacity: 0%;
+  color: $main-brand;
+  font-family: 'Georgia', sans-serif;
+  font-weight:500;
+  font-size: 50px;
+  text-align: right;
+  float: right;
+  animation: 2s linear 1s reaveal forwards;
+  margin-left:-355px;
 }
 
-#nav {
-  padding: 30px;
+.title-line {
+    content: '';
+    margin-top: 37px;
+    border-top: 2px solid;
+    flex: 1 0 10px;
+    color: $main-brand;
+    animation: slidein 2s;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+//TODO: Maybe move these to their own animations file
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@keyframes reaveal {
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+}
+
+@keyframes slidein {
+  0% {
+    margin-right: 800px;
+  }
+  100% {
+    margin-left:0px;
   }
 }
+
 </style>
